2183202574c758eb1b001f82a5d933278c3befbc
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-10-25 10:39:13 +0200

    Added code

--- functions-example/js/index.js
+++ functions-example/js/index.js
@@ -1,5 +1,6 @@
 const inputElem = document.querySelector('#number');
-
+const secondInput = document.querySelector('#secondNumber');
+const buttonElem = document.querySelector('#calculate');
 /**
  * Tar två siffror som argument och returneras dess summa
  */
@@ -11,9 +12,19 @@ function add(firstNumber, secondNumber) {
 
 function showSum(sum) {
     console.log('Summan blev:', sum);
+    alert(`Summan blev: ${sum}`);
 }
 
-const mySum = add(5, 5);
-showSum(mySum); // Skickar med värdet som argument i variabeln mySum
-const newSum = add(20, 20);
-showSum(newSum);
+buttonElem.addEventListener('click', function() {
+    const firstNumber = parseInt(inputElem.value);
+    const secondNumber = parseInt(secondInput.value);
+
+    const result = add(firstNumber, secondNumber);
+    showSum(result);
+});
+
+
+// const mySum = add(5, 5);
+// showSum(mySum); // Skickar med värdet som argument i variabeln mySum
+// const newSum = add(20, 20);
+// showSum(newSum);
